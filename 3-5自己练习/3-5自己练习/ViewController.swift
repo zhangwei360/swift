a726ebcde1fb53f787755d14e3442fd22481476e
zhangwei360 <45582385+zhangwei360@users.noreply.github.com>
2019-04-15 19:10:49 +0900

    Update ViewController.swift

--- 3-5自己练习/3-5自己练习/ViewController.swift
+++ 3-5自己练习/3-5自己练习/ViewController.swift
@@ -15,13 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.delegate = self
-        tableView.dataSource = self
         
-        tableView.layer.shadowColor = UIColor.black.cgColor
-        tableView.layer.shadowOffset = CGSize(width: 2, height: 2)
-        tableView.layer.shadowRadius = 0
-        tableView.layer.shadowOpacity = 0.5
         
     }
 
@@ -104,16 +98,4 @@ class ViewController: UIViewController {
     
 
 }
-extension ViewController:UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "name", for: indexPath) as! NameTableViewCell
-        cell.nameLabel.text = "123456"
-        return cell
-    }
-    
-    
-}
+
