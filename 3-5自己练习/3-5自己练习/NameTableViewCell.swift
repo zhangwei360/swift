d5112c8aef31fd99e1d6b378f95f474476a6d1e7
zhangwei360 <45582385+zhangwei360@users.noreply.github.com>
2019-05-15 17:01:27 +0900

    Update NameTableViewCell.swift

--- 3-5自己练习/3-5自己练习/NameTableViewCell.swift
+++ 3-5自己练习/3-5自己练习/NameTableViewCell.swift
@@ -20,3 +20,48 @@ class NameTableViewCell: UITableViewCell {
    
 https://stackoverflow.com/questions/42723130/crashed-com-apple-root-default-qos
 }
+
+<ImagerControlInformationChangeTable>
+　　　<ImagerControlInformationChange frameCount=“0”     status=“start” />
+　　　<ImagerControlInformationChange frameCount=“100” status=“none” />
+　</ImagerControlInformationChangeTable>
+　<LensControlInformationChangeTable>
+　　　<LensControlInformationChange frameCount=“0”     status=“start” />
+　　　<LensControlInformationChange frameCount=“100” status=“none” />
+　</LensControlInformationChangeTable>
+　<DistortionCorrectionChangeTable>
+　　　<DistortionCorrectionChange frameCount=“0”     status=“start” />
+　　　<DistortionCorrectionChange frameCount=“100” status=“none” />
+　</DistortionCorrectionChangeTable>
+　<GyroscopeChangeTable>
+　　　<GyroscopeChange frameCount=“0”     status=“start” />
+　　　<GyroscopeChange frameCount=“100” status=“none” />
+　</GyroscopeChangeTable>
+　<AccelerometerChangeTable>
+　　　<AccelerometerChange frameCount=“0”     status=“start” />
+　　　<AccelerometerChange frameCount=“100” status=“none” />
+　</AccelerometerChangeTable>
+
+
+
+
+<ChangeTable name="ImagerControlInformation">
+　　　<Event status="start" frameCount="0"/>
+　　　<Event status="none" frameCount=“100"/>
+　　</ChangeTable>
+　　<ChangeTable name="LensControlInformation">
+　　　<Event status="start" frameCount="0"/>
+　　　<Event status="none" frameCount=“100"/>
+　　</ChangeTable>
+　　<ChangeTable name="DistortionCorrection">
+　　　<Event status="start" frameCount="0"/>
+　　　<Event status="none" frameCount=“100"/>
+　　</ChangeTable>
+　　<ChangeTable name="Gyroscope">
+　　　<Event status="start" frameCount="0"/>
+　　　<Event status="none" frameCount=“100"/>
+　　</ChangeTable>
+　　<ChangeTable name="Accelerometor">
+　　　<Event status="start" frameCount="0"/>
+　　　<Event status="none" frameCount=“100"/>
+　　</ChangeTable>
